0ec39ed554a050099bb1094a0b59eec5d7362e95
Simon Bloom <swbloom@gmail.com>
2017-04-22 14:49:33 -0400

    spaces instead of tabs, adds eslint

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express			= require('express');
-const app				= express();
-const bodyParser		= require('body-parser');
-const mongoose			= require('mongoose');
-const Pet 				= require('./models/pet.js');
+const express     = require('express');
+const app       = express();
+const bodyParser    = require('body-parser');
+const mongoose      = require('mongoose');
+const Pet         = require('./models/pet.js');
 
 mongoose.connect('mongodb://localhost:27017/updog');
 
@@ -14,78 +14,78 @@ const port = process.env.PORT || 8080;
 const router = express.Router();
 
 router.use((req, res, next) => {
-	console.log('middleware ->');
-	next();
+  console.log('middleware ->');
+  next();
 });
 
 router.route('/pets')
-	.post((req, res) => {
-		const pet = new Pet();
+  .post((req, res) => {
+    const pet = new Pet();
 
-		pet.name = req.body.name; 
-		pet.score = 0 // all pets start with a base score of 0
-		pet.description = req.body.description;
-		pet.photo = req.body.photo;
+    pet.name = req.body.name; 
+    pet.score = 0 // all pets start with a base score of 0
+    pet.description = req.body.description;
+    pet.photo = req.body.photo;
 
-		pet.save((err, doc) => {
-			if (err) {
-				res.send(err);
-			}
+    pet.save((err, doc) => {
+      if (err) {
+        res.send(err);
+      }
 
-			res.json(doc);
-		});
-	})
-	.get((req, res) => {
-		Pet.find((err, pets) => {
-			if (err) {
-				res.send(err);
-			}
-			res.json(pets);
-		});
+      res.json(doc);
+    });
+  })
+  .get((req, res) => {
+    Pet.find((err, pets) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(pets);
+    });
 });
 
 router.route('/pets/:pet_id')
-	.get((req, res) => {
-		Pet.findById(req.params.pet_id, (err, pet) => {
-			if (err) {
-				res.send(err);
-			}
-			res.json(pet);
-		});
-	})
-	.put((req, res) => {
-		Pet.findById(req.params.pet_id, (err, pet) => {
-			if (err) {
-				res.send(err);
-			}
+  .get((req, res) => {
+    Pet.findById(req.params.pet_id, (err, pet) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(pet);
+    });
+  })
+  .put((req, res) => {
+    Pet.findById(req.params.pet_id, (err, pet) => {
+      if (err) {
+        res.send(err);
+      }
 
-			pet.name = req.body.name;
-			pet.score = req.body.score;
-			pet.description = req.body.description;
-			pet.photo = req.body.photo;
-			
-			pet.save((err, doc) => {
-				if (err) {
-					res.send(err);
-				}
-				res.json(doc);
-			});
-		});
-	})
-	.delete((req, res) => {
-		Pet.remove({
-			_id: req.params.pet_id
-		}, (err, pet) => {
-			if (err) {
-				res.send(err);
-			}
+      pet.name = req.body.name;
+      pet.score = req.body.score;
+      pet.description = req.body.description;
+      pet.photo = req.body.photo;
+      
+      pet.save((err, doc) => {
+        if (err) {
+          res.send(err);
+        }
+        res.json(doc);
+      });
+    });
+  })
+  .delete((req, res) => {
+    Pet.remove({
+      _id: req.params.pet_id
+    }, (err, pet) => {
+      if (err) {
+        res.send(err);
+      }
 
-			res.json({ message: 'Successfully deleted'});
-		});
-	});
+      res.json({ message: 'Successfully deleted'});
+    });
+  });
 
 router.get('/', (req, res) => {
-	res.json({ message: `What's up, dog?`});
+  res.json({ message: `What's up, dog?`});
 });
 
 

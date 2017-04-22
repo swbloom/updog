20dd621e475f478348ec79e5c843fdb471def32b
Simon Bloom <swbloom@gmail.com>
2017-04-22 13:44:50 -0400

    adds express, mongoose, models, first endpoints

---
+++ models/pet.js
@@ -0,0 +1,11 @@
+const mongoose			= require('mongoose');
+const Schema			= mongoose.Schema;
+
+const PetSchema = new Schema({
+	name: String,
+	photo: String,
+	description: String,
+	score: Number
+});
+
+module.exports = mongoose.model('Pet', PetSchema);
---
+++ server.js
@@ -0,0 +1,95 @@
+const express			= require('express');
+const app				= express();
+const bodyParser		= require('body-parser');
+const mongoose			= require('mongoose');
+const Pet 				= require('./models/pet.js');
+
+mongoose.connect('mongodb://localhost:27017/updog');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+	console.log('middleware ->');
+	next();
+});
+
+router.route('/pets')
+	.post((req, res) => {
+		const pet = new Pet();
+
+		pet.name = req.body.name; 
+		pet.score = 0 // all pets start with a base score of 0
+		pet.description = req.body.description;
+		pet.photo = req.body.photo;
+
+		pet.save((err, doc) => {
+			if (err) {
+				res.send(err);
+			}
+
+			res.json(doc);
+		});
+	})
+	.get((req, res) => {
+		Pet.find((err, pets) => {
+			if (err) {
+				res.send(err);
+			}
+			res.json(pets);
+		});
+});
+
+router.route('/pets/:pet_id')
+	.get((req, res) => {
+		Pet.findById(req.params.pet_id, (err, pet) => {
+			if (err) {
+				res.send(err);
+			}
+			res.json(pet);
+		});
+	})
+	.put((req, res) => {
+		Pet.findById(req.params.pet_id, (err, pet) => {
+			if (err) {
+				res.send(err);
+			}
+
+			pet.name = req.body.name;
+			pet.score = req.body.score;
+			pet.description = req.body.description;
+			pet.photo = req.body.photo;
+			
+			pet.save((err, doc) => {
+				if (err) {
+					res.send(err);
+				}
+				res.json(doc);
+			});
+		});
+	})
+	.delete((req, res) => {
+		Pet.remove({
+			_id: req.params.pet_id
+		}, (err, pet) => {
+			if (err) {
+				res.send(err);
+			}
+
+			res.json({ message: 'Successfully deleted'});
+		});
+	});
+
+router.get('/', (req, res) => {
+	res.json({ message: `What's up, dog?`});
+});
+
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Updog is wagging its tail on port ' + port);
